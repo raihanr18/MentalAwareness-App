07904f5996e3dc906337041047edd433c82e1bf6
fernaldytolol <155967997+fernaldytolol@users.noreply.github.com>
2024-02-16 21:10:50 +0700

    Update meditasi.dart

--- lib/pages/meditasi/meditasi.dart
+++ lib/pages/meditasi/meditasi.dart
@@ -1,57 +1,63 @@
 import 'package:flutter/material.dart';
-import 'track.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-class MeditasiPage extends StatelessWidget {
-  const MeditasiPage({super.key});
+class AudioManager {
+  static final AudioPlayer _audioPlayer = AudioPlayer();
+  static String? _currentAudioFile;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.indigoAccent,
-      body: ListView(
-        children: const [
-          MeditasiListItem(
-            title: 'Meditasi Untuk Mencapai Pencerahan',
-            audioFile: 'music/pencerahan.mp3',
-            icon: Icons.lightbulb,
-          ),
-          MeditasiListItem(
-            title: 'Meditasi Untuk Yang Sedang Bersedih',
-            audioFile: 'music/sedih.mp3',
-            icon: Icons.sentiment_very_dissatisfied,
-          ),
-          MeditasiListItem(
-            title: 'Meditasi Untuk Seseorang Yang Sedang banyak Pikiran',
-            audioFile: 'music/stress.mp3',
-            icon: Icons.warning,
-          ),
-          MeditasiListItem(
-            title: 'Meditasi Untuk Yang Sedang Tidak Percaya Diri',
-            audioFile: 'music/tidakpercaya.mp3',
-            icon: Icons.thumb_down_alt,
-          ),
-          MeditasiListItem(
-            title: 'Meditasi Untuk Yang Susah Tidur',
-            audioFile: 'music/tidur.mp3',
-            icon: Icons.nightlight_round,
-          ),
-          MeditasiListItem(
-            title: 'Untuk Kamu Yang Sedang Hilang Harapan',
-            audioFile: 'music/hilangharapan.mp3',
-            icon: Icons.healing,
-          ),
-        ],
-      ),
-    );
+  static final Map<String, bool> _isPlayingMap = {};
+
+  static Future<void> playAudio(String audioFile, BuildContext context) async {
+    try {
+      if (_currentAudioFile != null && _currentAudioFile != audioFile) {
+        await _audioPlayer.stop();
+        _isPlayingMap[_currentAudioFile!] = false;
+      }
+
+      await _audioPlayer.play(AssetSource(audioFile));
+      _currentAudioFile = audioFile;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Memainkan $audioFile')),
+      );
+
+      _isPlayingMap[_currentAudioFile!] = true;
+    } catch (e) {
+      print('Error playing audio: $e');
+    }
+  }
+
+  static Future<void> stopAudio() async {
+    try {
+      await _audioPlayer.stop();
+      if (_currentAudioFile != null) {
+        _isPlayingMap[_currentAudioFile!] = false;
+      }
+      _currentAudioFile = null;
+    } catch (e) {
+      print('Error stopping audio: $e');
+    }
+  }
+
+  static bool isPlaying(String audioFile) {
+    return _isPlayingMap[audioFile] ?? false;
+  }
+
+  static Future<void> seekTo(Duration position) async {
+    try {
+      await _audioPlayer.seek(position);
+    } catch (e) {
+      print('Error seeking audio: $e');
+    }
   }
 }
 
-class MeditasiListItem extends StatefulWidget {
+class TrackPage extends StatelessWidget {
   final String title;
   final String audioFile;
   final IconData icon;
 
-  const MeditasiListItem({
+  const TrackPage({
     super.key,
     required this.title,
     required this.audioFile,
@@ -59,33 +65,164 @@ class MeditasiListItem extends StatefulWidget {
   });
 
   @override
-  _MeditasiListItemState createState() => _MeditasiListItemState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      backgroundColor: Colors.white,
+      body: TrackPageContent(
+        title: title,
+        audioFile: audioFile,
+        icon: icon,
+      ),
+    );
+  }
 }
 
-class _MeditasiListItemState extends State<MeditasiListItem> {
+class TrackPageContent extends StatefulWidget {
+  final String title;
+  final String audioFile;
+  final IconData icon;
+
+  const TrackPageContent({
+    super.key,
+    required this.title,
+    required this.audioFile,
+    required this.icon,
+  });
+
+  @override
+  _TrackPageContentState createState() => _TrackPageContentState();
+}
+
+class _TrackPageContentState extends State<TrackPageContent>
+    with SingleTickerProviderStateMixin {
+  late bool isPlaying;
+  Duration _duration = Duration();
+  Duration _position = Duration();
+
+  @override
+  void initState() {
+    super.initState();
+    isPlaying = AudioManager.isPlaying(widget.audioFile);
+    // Mendengarkan perubahan durasi audio
+    AudioManager._audioPlayer.onDurationChanged.listen((Duration duration) {
+      setState(() {
+        _duration = duration;
+      });
+    });
+    // Mendengarkan perubahan posisi audio
+    AudioManager._audioPlayer.onPositionChanged.listen((Duration position) {
+      setState(() {
+        _position = position;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    AudioManager.stopAudio();
+    super.dispose();
+  }
+
+  Future<void> _toggleAudio() async {
+    if (isPlaying) {
+      await AudioManager.stopAudio();
+    } else {
+      await AudioManager.playAudio(widget.audioFile, context);
+    }
+    setState(() {
+      isPlaying = !isPlaying;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      margin: const EdgeInsets.all(8),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => TrackPage(
-                title: widget.title,
-                audioFile: widget.audioFile,
-                icon: widget.icon,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: 250,
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.rectangle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Icon(
+              widget.icon,
+              size: 100,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          widget.title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          '${_position.inMinutes}:${(_position.inSeconds % 60).toString().padLeft(2, '0')} / ${_duration.inMinutes}:${(_duration.inSeconds % 60).toString().padLeft(2, '0')}',
+          style: TextStyle(fontSize: 24.0, color: Colors.blue),
+        ),
+        const SizedBox(height: 20),
+        Slider(
+            value: _position.inSeconds.toDouble(),
+            min: 0,
+            max: _duration.inSeconds.toDouble(),
+            onChanged: (newValue) {
+              if (mounted) {
+                setState(() {
+                  _position = Duration(seconds: newValue.toInt());
+                });
+              }
+            },
+            onChangeEnd: (newValue) {
+              if (mounted) {
+                AudioManager.seekTo(Duration(seconds: newValue.toInt()));
+              }
+            }),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isPlaying ? Colors.red : Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: () async {
+                await _toggleAudio();
+              },
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: isPlaying
+                    ? Icon(Icons.stop, size: 18)
+                    : Icon(Icons.play_arrow, size: 18),
               ),
             ),
-          );
-        },
-        child: ListTile(
-          leading: Icon(widget.icon, color: Colors.indigo),
-          title: Text(widget.title),
+          ],
         ),
-      ),
+      ],
     );
   }
 }
